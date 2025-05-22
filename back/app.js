a91a58c0f817a1619ac8b3df79dbe36623397b74
mister2dev <michelmaineime@hotmail.com>
2025-05-22 10:39:31 +0200

    maj mode demo auth

--- back/app.js
+++ back/app.js
@@ -15,14 +15,25 @@ app.use(express.json());
 
 //-------Routes---------------
 
+// app.use((req, res, next) => {
+//   if (req.method !== "GET") {
+//     return res
+//       .status(403)
+//       .json({ message: "Mode démo : écriture désactivée." });
+//   }
+//   next();
+// });
+
 app.use((req, res, next) => {
-  if (req.method !== "GET") {
+  const allowedPaths = ["/api/auth/login"];
+  if (req.method !== "GET" && !allowedPaths.includes(req.path)) {
     return res
       .status(403)
       .json({ message: "Mode démo : écriture désactivée." });
   }
   next();
 });
+
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/post", postRoutes);
